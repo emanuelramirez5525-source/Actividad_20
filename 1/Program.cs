26b69cac16518bc3f6ac75967d319894e81b48fc
emanu <emanu@EMANUEL_RAMREZ>
2026-04-08 23:19:16 -0600

    Programa Creado!

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Solicitar al usuario la cantidad n de números, guardarlos en un arreglo y mostrarlos en pantalla
