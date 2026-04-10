04d9c6e719926eedb0223c6b7962d8033239a601
emanu <emanu@EMANUEL_RAMREZ>
2026-04-09 21:17:47 -0600

    Logica Aplicada!

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,2 +1,28 @@
 ﻿// Solicitar n números, guardarlos en un arreglo y mostrarlos en orden inverso
 
+Console.WriteLine("Ingrese la cantidad de números que quiera guardar:");
+int n;
+
+while (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+{
+    Console.WriteLine("Error ingrese un número válido mayor que 0");
+}
+
+int[] numeros = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine($"Ingrese el número {i + 1}");
+
+    while (!int.TryParse(Console.ReadLine(), out numeros[i]))
+    {
+        Console.WriteLine("Ingrese un número válido");
+    }
+}
+
+Console.WriteLine("Los números ingresados son:");
+
+for (int i = n - 1; i >= 0; i--)
+{
+    Console.WriteLine(numeros[i]);
+}
